2e154d75a43d8c8299bb77fa963a12122287a1c2
natepetta1 <crazyna7e@hotmail.com>
2026-08-07 13:49:48 -0400

    Update header.js

--- header.js
+++ header.js
@@ -1,4 +1,17 @@
 const navig = document.querySelector('#navigationgoeshere');
 window.AddEventListener('load', function () {
-  navig.innerHTML = 
+  navig.innerHTML = `<nav id="linknavigation">
+  <span class="mark">NATE SHADOW</span>
+  <span class="links">
+    <span class="dropdown">
+      <a href="/Nate-Shadow/ghoul.html">Freaks</a>
+      <span class="dropdown-menu">
+        <a href="/Nate-Shadow/ghoul.html">The Ghoul</a>
+        <a href="/Nate-Shadow/ghoul2.html">Ghoul2</a>
+      </span>
+    </span>
+    <a href="#story">Shadow Manor</a>
+    <a href="#music">Music</a>
+  </span>
+</nav>`
 });
